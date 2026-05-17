10d723538aa9fb70a6b8db821b26968bb1683160
alshomok <aamed5898@gmail.com>
2026-05-17 11:56:42 +0200

    Delete src directory

--- src/app/chat-page/page.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ChatPageClient from './components/ChatPageClient';
-
-export default function ChatPage() {
-  return <ChatPageClient />;
-}
--- src/app/sign-up-login-screen/page.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import AuthScreen from './components/AuthScreen';
-
-export default function SignUpLoginPage() {
-  return <AuthScreen />;
-}
